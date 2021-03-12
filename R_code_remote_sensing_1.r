d406f55d9ad4a65d8c72f08e6a0ae317fbdb2dc5
mecavallini <80532837+mecavallini@users.noreply.github.com>
2021-03-12 18:02:39 +0100

    Create R_code_remote_sensing_1.r

---
+++ R_code_remote_sensing_1.r
@@ -0,0 +1,13 @@
+#codice in R per telerilevamento
+
+setwd("/Users/mariaelenacavallini/lab/") #con le virgolette perche' fuori da R
+
+#installo e importo raster
+#install.packages("raster") #gia installato
+library(raster)
+
+#funzione brick crea un oggetto raster brick
+p224r63_2011 <- brick("p224r63_2011_masked.grd")
+
+plot(p224r63_2011)
+dev.off()
