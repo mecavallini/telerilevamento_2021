1b43c8431d9e6d842585c6372e31efb087ef3d49
mecavallini <80532837+mecavallini@users.noreply.github.com>
2021-03-13 09:50:27 +0100

    Update R_code_remote_sensing_1.r

--- R_code_remote_sensing_1.r
+++ R_code_remote_sensing_1.r
@@ -9,5 +9,5 @@ library(raster)
 #funzione brick crea un oggetto raster brick
 p224r63_2011 <- brick("p224r63_2011_masked.grd")
 
-plot(p224r63_2011)
+plot(p224r63_2011) #plot delle 7 bande del file p22...
 dev.off()
