9499903e3038a2c6d200c95e139705121a4b3494
mecavallini <80532837+mecavallini@users.noreply.github.com>
2021-03-24 12:01:16 +0100

    Update R_code_remote_sensing_1.r

--- R_code_remote_sensing_1.r
+++ R_code_remote_sensing_1.r
@@ -14,3 +14,28 @@ plot(p224r63_2011) #plot delle 7 bande del file p22...
 
 cl <- colorRampPalette (c("orange","green","pink","purple","gold")) (100)
 plot(p224r63_2011, col=cl)
+
+# B1: blue
+# B2: green
+# B3: red
+# B4: NIR infrarosso vicino
+# B5 infrarosso medio
+# B6 infrarosso termico
+# B7 infrarosso medio
+
+#dev off pulisce il grafico attuale
+dev.off()
+#plot solo della banda del blu
+cls <- colorRampPalette(c("red","pink","orange","purple")) (200)
+plot(p224r63_2011$B1_sre, col=cls)
+
+dev.off()
+#funzione par:  mf(multiframe) 1 riga e 2 colonne
+par(mfrow=c(1,2))
+clb <- colorRampPalette(c('dark blue','blue','light blue'))(100) # 
+plot(p224r63_2011$B1_sre, col=clb)
+
+# 2 row, 1 columns
+par(mfrow=c(2,1))
+plot(p224r63_2011$B1_sre)
+plot(p224r63_2011$B2_sre)
