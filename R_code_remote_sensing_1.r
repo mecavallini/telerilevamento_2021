fd0ed334d73783299ef1190a818b48326e4897cd
mecavallini <80532837+mecavallini@users.noreply.github.com>
2021-03-31 12:32:07 +0200

    Update R_code_remote_sensing_1.r

--- R_code_remote_sensing_1.r
+++ R_code_remote_sensing_1.r
@@ -99,3 +99,17 @@ plotRGB(p224r63_2011, r=3, g=2, b=1, stretch="lin") #colori naturali
 plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="lin") #falsi colori lineari
 #stretch histogram
 plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="hist") #falsi colori histograms
+
+#31.03
+#multitemporal set
+# 2011 image p224r63_2011_masked
+p224r63_2011 <- brick("p224r63_2011_masked.grd")
+# 1988 image p224r63_1988_masked
+p224r63_1988 <- brick("p224r63_1988_masked.grd")
+pdf("4imm2011-1988.pdf")
+par(mfrow=c(2,2)) #per visualizzare insieme
+plotRGB(p224r63_1988, r=4, g=3, b=2, stretch="lin") #colori
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="lin") #colori
+plotRGB(p224r63_1988, r=4, g=3, b=2, stretch="hist")
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="hist")
+dev.off()
