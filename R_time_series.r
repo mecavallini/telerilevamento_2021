06320433c120d92e85da445b74d7d19e2ac7446c
mecavallini <80532837+mecavallini@users.noreply.github.com>
2021-03-31 15:24:44 +0200

    Update R_time_series.r

--- R_time_series.r
+++ R_time_series.r
@@ -1,1 +1,8 @@
+# time series analysis
+# greenland increase of temperature
+# data e codice di Emanuela Cosma
 
+# install.packages("raster")
+library(raster)
+
+setwd("/Users/mariaelenacavallini/Desktop/lab/greenland") 
