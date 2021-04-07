47cf71ea70f7ac159fdb942a78a563a65dd40bb6
mecavallini <80532837+mecavallini@users.noreply.github.com>
2021-04-07 12:23:57 +0200

    Update R_time_series.r

--- R_time_series.r
+++ R_time_series.r
@@ -4,5 +4,8 @@
 
 # install.packages("raster")
 library(raster)
-
 setwd("/Users/mariaelenacavallini/lab/greenland") 
+lst_2000 <- raster("lst_2000.tif")
+#plot(lst_2000) per visualizzare la prima mappa caricata
+lst_2005 <- raster("lst_2005.tif")
+#plot(lst_2005)
