3cf48b9be8622e67e13ba356697b0bf9152db6df
mecavallini <80532837+mecavallini@users.noreply.github.com>
2021-04-09 10:53:41 +0200

    Update R_time_series.r

--- R_time_series.r
+++ R_time_series.r
@@ -4,6 +4,9 @@
 
 # install.packages("raster")
 library(raster)
+#rasterVis: metodi di visualizzazione per i dati raster
+library(rasterVis)
+
 setwd("/Users/mariaelenacavallini/lab/greenland") 
 lst_2000 <- raster("lst_2000.tif")
 #plot(lst_2000) per visualizzare la prima mappa caricata
@@ -21,8 +24,29 @@ dev.off()
 
 #list f file:
 rlist <- list.files(pattern="lst") #vado solo a trovare i file con nome "lst"
-import <- lapply(rlist,raster) #così li importo tutti insieme lapply
+import <- lapply(rlist,raster) #così li importo tutti insieme lapply, con funzione raster che importa singoli file 
 TGr <- stack(import) #fa un blocco unico di file raster in un vettore
 plot(TGr)
-plotRGB(TGr,1,2,3,stretch="Lin") #primo livello 00, 2 05, 3 10 RGB 
+plotRGB(TGr,1,2,3,stretch="Lin") #primo livello 2000, 2 2005, 3 2010 RGB 
 #plotRGB(TGr,2,3,4,stretch="Lin")
+
+levelplot(TGr)
+levelplot(TGr$lst_2000)
+
+cl <- colorRampPalette(c("blue","light blue","pink","red"))(100)
+levelplot(TGr, col.regions=cl)
+levelplot(TGr,col.regions=cl, names.attr=c("July 2000","July 2005", "July 2010", "July 2015"))
+levelplot(TGr,col.regions=cl, main="LST variation in time",
+          names.attr=c("July 2000","July 2005", "July 2010", "July 2015"))
+
+#melt
+meltlist <- list.files(pattern="melt") #vado solo a trovare i file con nome "lst"
+mimport <- lapply(meltlist,raster) #così li importo tutti insieme lapply, con funzione raster che importa singoli file 
+MLT <- stack(mimport) 
+cl <- colorRampPalette(c("blue","light blue","pink","red"))(100)
+levelplot(MLT,col.regions=cl, main="melt variation in time")
+          #names.attr=c("July 2000","July 2005", "July 2010", "July 2015"))
+#differenze di scioglimento
+diff <- MLT$X2007annual_melt - MLT$X1979annual_melt #melt$ per dire che appartiene a MLT
+cld <- colorRampPalette(c("blue","white","red"))(100)
+levelplot(diff,col.regions=cld, main="melt variation 2007-1979")
