e0a28c22575c7d1c493a535fab35cf97db3a1510
mecavallini <80532837+mecavallini@users.noreply.github.com>
2021-04-17 17:12:59 +0200

    Update R_code_knitr.r

--- R_code_knitr.r
+++ R_code_knitr.r
@@ -1,8 +1,8 @@
 # starting from the code folder where framed.sty is put
 
-#setwd("/Users/mariaelenacavallini/lab/") # Mac
-setwd("C:/lab") # Windows
+setwd("/Users/mariaelenacavallini/lab/") # Mac
+#setwd("C:/lab") # Windows
 #require(knitr)
 library(knitr)
 
-stitch("gr.r", template=system.file("misc", "knitr-template.Rnw", package="knitr"))
+stitch("R_code_greenland.r", template=system.file("misc", "knitr-template.Rnw", package="knitr"))
