334619d0fb5d4127b59d204b1f6bb46a06ace608
mecavallini <80532837+mecavallini@users.noreply.github.com>
2021-04-23 10:48:52 +0200

    Update R_code_multivariate_analysis.r

--- R_code_multivariate_analysis.r
+++ R_code_multivariate_analysis.r
@@ -1,2 +1,10 @@
 setwd("/Users/mariaelenacavallini/lab/") # Mac
 #setwd("C:/lab") # Windows
+
+library(raster)
+library(RStoolbox)
+#caricamento immagine p224r63_2011_masked.grd
+p224r63_2011 <- brick("p224r63_2011_masked.grd")
+plot(p224r63_2011)
+
+plot(p224r63_2011$B1_sre,p224r63_2011$B2_sre, col="red", pch=19, cex=2)
