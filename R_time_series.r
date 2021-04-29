67a5dbc46a472d84da81ba3317eb333d3182fea1
mecavallini <80532837+mecavallini@users.noreply.github.com>
2021-04-29 16:19:40 +0200

    Update R_time_series.r

--- R_time_series.r
+++ R_time_series.r
@@ -2,6 +2,7 @@
 # greenland increase of temperature
 # data e codice di Emanuela Cosma
 
+#importo o installo le librerie necessarie
 #install.packages("raster")
 library(raster)
 #rasterVis: metodi di visualizzazione per i dati raster
@@ -9,12 +10,14 @@ library(raster)
 library(rasterVis)
 library(rgdal)
 
+#imposto la working directory
 #setwd("C:/lab/greenland") # Windows
 setwd("/Users/mariaelenacavallini/lab/greenland") 
+
+#importo i file .tif tramite la funzione raster
 lst_2000 <- raster("lst_2000.tif")
 #plot(lst_2000) per visualizzare la prima mappa caricata
 lst_2005 <- raster("lst_2005.tif")
-#plot(lst_2005)
 lst_2010 <- raster("lst_2010.tif")
 lst_2015 <- raster("lst_2015.tif")
 
@@ -30,7 +33,7 @@ rlist <- list.files(pattern="lst") #vado solo a trovare i file con nome "lst"
 import <- lapply(rlist,raster) #così li importo tutti insieme lapply, con funzione raster che importa singoli file 
 TGr <- stack(import) #fa un blocco unico di file raster in un vettore
 plot(TGr)
-plotRGB(TGr,1,2,3,stretch="Lin") #primo livello 2000, 2 2005, 3 2010 RGB 
+plotRGB(TGr,1,2,3,stretch="Lin") #primo livello 2000, 2° 2005, 3° 2010 RGB 
 #plotRGB(TGr,2,3,4,stretch="Lin")
 
 levelplot(TGr)
@@ -42,13 +45,13 @@ levelplot(TGr,col.regions=cl, names.attr=c("July 2000","July 2005", "July 2010",
 levelplot(TGr,col.regions=cl, main="LST variation in time",
           names.attr=c("July 2000","July 2005", "July 2010", "July 2015"))
 
-#melt
+#melt variation in time
 meltlist <- list.files(pattern="melt") #vado solo a trovare i file con nome "lst"
 mimport <- lapply(meltlist,raster) #così li importo tutti insieme lapply, con funzione raster che importa singoli file 
 MLT <- stack(mimport) 
 cl <- colorRampPalette(c("blue","light blue","pink","red"))(100)
 levelplot(MLT,col.regions=cl, main="melt variation in time")
-          #names.attr=c("July 2000","July 2005", "July 2010", "July 2015"))
+          #names.attr=c("July 2000","July 2005", "July 2010", "July 2015", ...))
 #differenze di scioglimento
 diff <- MLT$X2007annual_melt - MLT$X1979annual_melt #melt$ per dire che appartiene a MLT
 cld <- colorRampPalette(c("blue","white","red"))(100)
