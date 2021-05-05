be8253366e5c5abdabc0105021d4ca33474d7a89
mecavallini <80532837+mecavallini@users.noreply.github.com>
2021-05-05 12:26:34 +0200

    Update LandCover.r

--- LandCover.r
+++ LandCover.r
@@ -1,5 +1,16 @@
 #LandCover.r
 
+#library
+library(ggplot2)
+library(raster)
+library(RStoolbox) #classification
+
+
+
 setwd("/Users/mariaelenacavallini/lab/") # Mac
 #setwd("C:/lab") # Windows
+
+#caricamento delle 2 immagini
+defor1 <- brick("defor1.jpg") 
+defor2 <- brick("defor2.jpg") 
 
