21cdc74e281311c5e5efbccea66b00ebb668756c
mecavallini <80532837+mecavallini@users.noreply.github.com>
2021-05-19 15:56:02 +0200

    Update R_Code_variability.r

--- R_Code_variability.r
+++ R_Code_variability.r
@@ -31,3 +31,14 @@ plot(ndvisd3, col=clsd)
 ndvimn <- focal(ndvi, w=matrix(1/9, nrow=3,ncol=3), fun=mean)
 clsd <- colorRampPalette(c('blue','green','pink','magenta','orange','brown','red','yellow'))(100) #
 plot(ndvimn, col=clsd)
+
+ndvisd13 <- focal(ndvi, w=matrix(1/25, nrow=5,ncol=5), fun=sd)
+clsd <- colorRampPalette(c('blue','green','pink','magenta','orange','brown','red','yellow'))(100) #
+plot(ndvisd13, col=clsd)
+
+#PCA analisi
+sentinel_pca <- rasterPCA(sent)
+sentinel_pca
+plot(sentinel_pca$map) #dalla prima alla 4°immagine diminuisce la quantità di informazioni
+summary(sentinel_pca$model)
+#il primo pc contiene  67,36804%   di informazione
