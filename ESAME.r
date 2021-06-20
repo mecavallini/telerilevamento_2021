95103b2b99656d1bffd46473d386e02c28ffe6a2
mecavallini <80532837+mecavallini@users.noreply.github.com>
2021-06-20 11:28:32 +0200

    Update ESAME.r

--- ESAME.r
+++ ESAME.r
@@ -1,14 +1,9 @@
 #codice utilizzato per l'esame                              Maria Elena Cavallini
 
+#analisi di immagini tra 2011 - 2018 - 2020 nel bacino del Gadria (Bz) 
+#classifico detrito vegetato e non vegetato e sua variazione nel tempo - frequenza delle 3 classi definite e relativi grafici (ggplot2) 
+#variazione ndvi nel tempo 2020 - 2011
 
-#GUArda da sito https://maps.civis.bz.it/#/GVCC_INTERNET/?lang=de&login=false&context=false
-#per fare ndvi
-
-
-#analisi di immagini tra 2011 -2020 nel bacino del Gadria (Bz) 
-#classifico detrito vegetato e non vegetato e sua variazione nel tempo - frequenza delle classi (tipo landcover) e relativi grafici
-#variazione ndvi nel tempo
-#ggplot 
 # fonte del dato “volo LiDAR unibz - progetto EARFLOW”
 
 
@@ -21,9 +16,13 @@ library(raster)
 #rasterVis: metodi di visualizzazione per i dati raster
 #install.packages("rasterVis")
 library(rasterVis)
+#install.packages("rgdal")
 library(rgdal)
+#install.packages("RStoolbox")
 library(RStoolbox)
+#install.packages("ggplot2")
 library(ggplot2)  
+#install.packages("gridExtra")
 library(gridExtra)
 
 #importo i file .tif tramite la funzione raster
@@ -32,7 +31,7 @@ g_18 <- brick("gadria18.tif")
 g_11 <- brick("gadria11.tif")
 g_20 <- brick("gadria20.tif")
 
-jpeg("/Users/mariaelenacavallini/lab/ESAME/parRGB.jpg", 800, 800)
+#jpeg("/Users/mariaelenacavallini/lab/ESAME/parRGB.jpg", 800, 800)
 par(mfrow=c(1,3))
 plotRGB(g_11, r=1, g=2, b=3, stretch="lin", main = "Plot 2011") 
 plotRGB(g_18, r=1, g=2, b=3, stretch="lin", main = "Plot 2018") 
@@ -46,24 +45,24 @@ cl2 <- colorRampPalette(c('light green','goldenrod1','brown'))(100)
 #brown detrito più "vecchio"
 
 CLg11 <- unsuperClass(g_11, nClasses=3)
-jpeg("/Users/mariaelenacavallini/lab/ESAME/CLg11.jpg", 800, 800)
+#jpeg("/Users/mariaelenacavallini/lab/ESAME/CLg11.jpg", 800, 800)
 plot(CLg11$map,col=cl2)
 
 CLg18 <- unsuperClass(g_18, nClasses=3)
-jpeg("/Users/mariaelenacavallini/lab/ESAME/CLg18.jpg", 800, 800)
+#jpeg("/Users/mariaelenacavallini/lab/ESAME/CLg18.jpg", 800, 800)
 plot(CLg18$map,col=cl2)
 
 CLg20 <- unsuperClass(g_20, nClasses=3)
-jpeg("/Users/mariaelenacavallini/lab/ESAME/CLg20.jpg", 800, 800)
+#jpeg("/Users/mariaelenacavallini/lab/ESAME/CLg20.jpg", 800, 800)
 plot(CLg20$map,col=cl2)
 
-jpeg("/Users/mariaelenacavallini/lab/ESAME/parCL.jpg", 800, 800)
+#jpeg("/Users/mariaelenacavallini/lab/ESAME/parCL.jpg", 800, 800)
 par(mfrow=c(1,3))
 plot(CLg11$map,col=cl2)
 plot(CLg18$map,col=cl2)       #i colori sono invertiti
 plot(CLg20$map,col=cl2)
 
-jpeg("/Users/mariaelenacavallini/lab/ESAME/parCL_rgb.jpg", 800, 800)
+#jpeg("/Users/mariaelenacavallini/lab/ESAME/parCL_rgb.jpg", 800, 800)
 par(mfrow=c(3,2))
 plot(CLg11$map,col=cl)
 plot(CLg18$map,col=cl)
@@ -136,37 +135,28 @@ pl18 <- ggplot(percentages, aes(x= cover, y=percent_18, color=cover)) + #color=c
 pl20 <- ggplot(percentages, aes(x= cover, y=percent_20, color=cover)) + #color=cover -> è la legenda
           geom_bar(stat="identity", fill="white")
 grid.arrange(pl11,pl18,pl20, nrow =1) #need gridExtra
+#jpeg("/Users/mariaelenacavallini/lab/ESAME/grafico_percentuali.jpg", 1600, 800)
 
-#################################### 
-setwd("/Users/mariaelenacavallini/lab/ESAME")
-
-#importo o installo le librerie necessarie
-#install.packages("raster")
-library(raster)
-#rasterVis: metodi di visualizzazione per i dati raster
-#install.packages("rasterVis")
-library(rasterVis)
-library(rgdal)
-library(RStoolbox)
-library(ggplot2)  
-library(gridExtra)
+####################################          Analisi della variazione di NDVI tra gli anni 2011 e 2020             ################################
+cl <- colorRampPalette(c('darkblue','yellow','red','black'))(100) # specifying a color scheme
 
 g_11R <- brick("gadria11R.png")
 #g_11R
-plotRGB(g_11R, r=1, g=2, b=3, stretch="lin")     
-cl <- colorRampPalette(c('darkblue','yellow','red','black'))(100) # specifying a color scheme
-
 g_20R <- brick("gadria20R.png")
 #g_20R
 
+plotRGB(g_11R, r=1, g=2, b=3, stretch="lin", main = "Plot in false color 2011") 
+plotRGB(g_18R, r=1, g=2, b=3, stretch="lin", main = "Plot in false color 2020") 
+
 #NDVI range (-1;+1) (NIR-RED)/ (NIR+RED)
 NDVI1= (g_11R$gadria11R.1 - g_11R$gadria11R.2)/(g_11R$gadria11R.1 + g_11R$gadria11R.2)
-jpeg("/Users/mariaelenacavallini/lab/ESAME/NDVI11.jpg", 800, 800)
+#jpeg("/Users/mariaelenacavallini/lab/ESAME/NDVI11.jpg", 800, 800)
 plot(NDVI1, col=cl, main="NDVI11")
 dev.off()
 NDVI2= (g_20R$gadria20R.1 - g_20R$gadria20R.2)/(g_20R$gadria20R.1 + g_20R$gadria20R.2)
-jpeg("/Users/mariaelenacavallini/lab/ESAME/NDVI20.jpg", 800, 800)
+#jpeg("/Users/mariaelenacavallini/lab/ESAME/NDVI20.jpg", 800, 800)
 plot(NDVI2, col=cl, main="NDVI20")
 dev.off()
 NDVIdiff <- NDVI2 - NDVI1
-plot(NDVIdiff, col=cl)
+#jpeg("/Users/mariaelenacavallini/lab/ESAME/NDVIdiff.jpg", 800, 800)
+plot(NDVIdiff, col=cl, main="NDVI20 - NDVI11")
