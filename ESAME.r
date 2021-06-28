1d50dd82cc3efd0cc02f8e2cfd3ce2b0878d827a
mecavallini <80532837+mecavallini@users.noreply.github.com>
2021-06-28 11:04:19 +0200

    Update ESAME.r

--- ESAME.r
+++ ESAME.r
@@ -82,9 +82,9 @@ s11 <- sum(46623578,41706884,75595846)
 prop11 <- f11/s11
 percent_11 <- prop11 * 100
 #            value    count
-#[1,] 6.100302e-07 28.44179 -->28%
-#[2,] 1.220060e-06 25.44246 -->26%
-#[3,] 1.830091e-06 46.11575 -->46%
+#[1,] 6.100302e-07 28.44179 -->28% verde bosco e prato
+#[2,] 1.220060e-06 25.44246 -->26% giallo roccia affiorante e detrito neoformato
+#[3,] 1.830091e-06 46.11575 -->46% marrone detrito più "vecchio"
 
 #frequenza 2018
 f18 <- freq(CLg18$map)
@@ -96,9 +96,9 @@ s18 <- sum(111364055,114230292,30544438)
 prop18 <- f18/s18
 percent_18 <- prop18 * 100
 #            value    count
-#[1,] 3.904133e-07 43.47801 -->43%
-#[2,] 7.808267e-07 44.59703 -->45%
-#[3,] 1.171240e-06 11.92496 -->12%
+#[1,] 3.904133e-07 43.47801 -->43% verde zone in ombra e bosco
+#[2,] 7.808267e-07 44.59703 -->45% giallo detrito più "vecchio"
+#[3,] 1.171240e-06 11.92496 -->12% marrone roccia affiorante e detrito neoformato
 
 #frequenza 2020
 f20 <- freq(CLg20$map)
@@ -110,9 +110,9 @@ s20 <- sum(85424235,92495860,78218690)
 prop20 <- f20/s20
 percent_20 <- prop20 * 100
 #            value    count
-#[1,] 3.904133e-07 33.35076 -->33%
-#[2,] 7.808267e-07 36.11162 -->36%
-#[3,] 1.171240e-06 30.53762 -->31%
+#[1,] 3.904133e-07 33.35076 -->33% verde bosco e prato
+#[2,] 7.808267e-07 36.11162 -->36% giallo roccia affiorante e detrito neoformato
+#[3,] 1.171240e-06 30.53762 -->31% marrone detrito più "vecchio"
 
 #generazione di un dataset (dataframe)
 #cl2 <- colorRampPalette(c('light green','goldenrod1','brown'))(100)
@@ -121,7 +121,7 @@ cover <- c('bosco e prato','roccia affiorante e detrito neoformato', 'detrito pi
 #goldenrod1 roccia affiorante e detrito neoformato
 #brown detrito più "vecchio"
 percent_11 <- c(28.44,25.44,46.12)
-percent_18 <- c(43.48,44.60,11.92)
+percent_18 <- c(43.48,11.92,44.60)
 percent_20 <- c(33.35,36.11,30.54)
 percentages <- data.frame(cover, percent_11, percent_18, percent_20) #column names
 percentages
@@ -158,7 +158,7 @@ g <- ggplot(data, aes(fill=anno, y=percentuali, x=copertura)) + geom_bar(positio
           geom_text(aes(label = percentuali),position=position_dodge(width=0.9), vjust=-0.25, size = 10) +
           My_Theme
 
-jpeg("/Users/mariaelenacavallini/lab/ESAME/grafico_percentualiGg.png", 1100, 800) #per il salvataggio
+jpeg("/Users/mariaelenacavallini/lab/ESAME/grafico_percentualiGg_ultimo.png", 1100, 800) #per il salvataggio
 g
 dev.off()
 
@@ -185,9 +185,115 @@ plot(NDVIdiff, col=cl, main="NDVI20 - NDVI11")
 ################################
 
 #importo i file .tif tramite la funzione raster
-#inserisco le foto degli anni 2011 - 2018 ------- da inserire anche gli anni 2020
+#inserisco le foto degli anni 2011 - 2020 
 ZI_11 <- brick("ZI11.tif")
-ZI_18 <- brick("ZI18.tif")
 ZI_20 <- brick("ZI20.tif")
 
-#fai classificazione a 2 
+#classificazione a 3
+set.seed(42) #per il numero random
+cl2 <- colorRampPalette(c('navyblue','chartreuse4','goldenrod1'))(100)
+#jpeg("/Users/mariaelenacavallini/lab/ESAME/CLg11.jpg", 800, 800)
+CLZI11 <- unsuperClass(ZI_11, nClasses=3)
+par(mfrow=c(2,1))
+plotRGB(ZI_11, r=1, g=2, b=3, stretch="lin", main = "Plot zona innesco 2011") 
+plot(CLZI11$map,col=cl2)
+#1navyblue zona in erosione
+#2chartreuse4 (verde) zona in ombra + bosco
+#3goldenrod1 zona più stabile
+
+CLZI20 <- unsuperClass(ZI_20, nClasses=3)
+#jpeg("/Users/mariaelenacavallini/lab/ESAME/CLg20.jpg", 800, 800)
+par(mfrow=c(2,1))
+plotRGB(ZI_20, r=1, g=2, b=3, stretch="lin", main = "Plot zona innesco 2011") 
+plot(CLZI20$map,col=cl2)
+#1navyblue zona in ombra
+#3goldenrod1 zona più stabile
+#2chartreuse4 (verde) zona di erosione
+
+par(mfrow=c(2,1)) #le due scale di colori non sono concordi
+plot(CLZI11$map,col=cl2)
+plot(CLZI20$map,col=cl2)
+
+###################################              Analisi di frequenza delle 3 classi                ################################
+#frequenza 2011
+f11zi <- freq(CLZI11$map)
+#     value    count
+#[1,]     1 1129363.  zona in erosione
+#[2,]     2 982999 zona in ombra + bosco
+#[3,]     3 1645631 zona più stabile
+
+#1navyblue zona in erosione
+#2chartreuse4 (verde) zona in ombra + bosco
+#3goldenrod1 zona più stabile
+
+s11zi <- sum(1129363,982999,1645631)
+prop11zi <- f11zi/s11zi
+zi_11 <- prop11zi * 100
+#            value    count
+#[1,] 2.660995e-05 30.05229
+#[2,] 5.321990e-05 26.15755
+#[3,] 7.982985e-05 43.79016
+
+#frequenza 2020
+f20zi <- freq(CLZI20$map)
+#     value    count
+#[1,]     1 1265031
+#[2,]     2 1954399
+#[3,]     3 2650552
+s20zi <- sum(1265031,1954399,2650552)
+prop20zi <- f20zi/s20zi
+zi_20 <- prop20zi * 100
+#            value    count
+#[1,] 1.703583e-05 21.55085  zona in ombra
+#[2,] 3.407165e-05 33.29480 zona di erosione
+#[3,] 5.110748e-05 45.15435 zona più stabile
+
+#zi_20 corretta
+#            value    count
+#[1,] 3.407165e-05 33.29480
+#[2,] 1.703583e-05 21.55085
+#[3,] 5.110748e-05 45.15435
+
+#1navyblue zona in ombra
+#3goldenrod1 zona più stabile
+#2chartreuse4 (verde) zona di erosione
+
+#generazione di un dataset (dataframe)
+#cl2 <- colorRampPalette(c('navyblue','chartreuse4','goldenrod1'))(100)
+cover <- c('zona in ombra','zona di erosione', 'zona più stabile')
+#navyblue zona in erosione
+#goldenrod1 zona più stabile
+#chartreuse4 (verde) zona di bosco
+Zpercent_11 <- c(30.05,26.16,43.79)
+Zpercent_20 <- c(33.30,21.55,45.15)
+Zpercentages <- data.frame(cover, Zpercent_11, Zpercent_20) #column names
+Zpercentages
+anno <- c("2011","2011","2011", "2020","2020","2020") 
+Zpercentuali <- c(Zpercent_11,Zpercent_20) 
+copertura <- cover 
+Zdata <- data.frame(anno,Zpercentuali,copertura)
+Zdata
+
+My_Theme = theme(
+  axis.title.x = element_text(size = 18),
+  axis.text.x = element_text(size = 18),
+  axis.title.y = element_text(size = 18),
+  legend.position="right", legend.background = element_rect(fill="lightblue", size=0.5, linetype="solid", colour ="black"), #“left”,“top”, “right”, “bottom”, “none”.
+  legend.key.size = unit(2, 'cm'),
+  legend.title=element_text(size=20), 
+  legend.text=element_text(size=20))
+
+ziipl11 <- ggplot(Zpercentages, aes(x= cover, y=Zpercent_11 , color=cover)) + #color=cover -> è la legenda
+          geom_bar(stat="identity", fill="white")
+ziipl20 <- ggplot(Zpercentages, aes(x= cover, y=Zpercent_20, color=cover)) + #color=cover -> è la legenda
+          geom_bar(stat="identity", fill="white")
+grid.arrange(ziipl11,ziipl20, nrow =1) #need gridExtra
+
+
+zipl <- ggplot(Zdata, aes(fill=anno, y=Zpercentuali, x=copertura)) + geom_bar(position="dodge", stat="identity") +
+          geom_text(aes(label = Zpercentuali),position=position_dodge(width=0.9), vjust=-0.25, size = 10) +
+          My_Theme
+jpeg("/Users/mariaelenacavallini/lab/ESAME/grafico_percentuali_zona_innesco.png", 1100, 800) #per il salvataggio
+zipl
+dev.off()
+
